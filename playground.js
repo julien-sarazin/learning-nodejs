1b1e0a401dea12f9e326258c2e3bddc4a035ebf0
Julien Sarazin <julien.sarazin@me.com>
2017-03-07 12:08:07 +0100

    updated playground.js

--- playground.js
+++ playground.js
@@ -1,42 +1,33 @@
 const http = require('http');
 const server = http.createServer();
+const path = '/users';
+const users = [];
+
+server.on('request', (request, response) => {
+  var body = [];
+  response.setHeader('Content-Type', 'application/json');
+
+  if (request.url != path || (request.method != 'POST' && request.method != 'GET')) {
+    response.statusCode = 404;
+    return response.end();
+  }
+
+  if (request.method == 'GET') {
+    return response.end(JSON.stringify(users));
+  }
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  })
+
+  request.on('end', () => {
+    let data = Buffer.concat(body).toString();
+    let json = JSON.parse(data);
+
+    users.push(json);
+    response.end(data);
+  });
+});
 
 console.log("server listening on port 3000");
 server.listen(3000);
-
-// Step #1.
-server.on('request', (req, res) => {
- res.writeHead(200, {
-   'Content-Type': 'text/plain'
- });
- res.end('It works!');
-});
-
-// Step #2.
-/*
-server.on('request', (request, response) => {
-    var body = [];
-    request
-        .on('data', (chunk) => {
-            body.push(chunk);
-        })
-        .on('end', function() {
-            body = Buffer.concat(body).toString();
-            console.log("body: ", body);
-        });
-});
-*/
-
-// Step #3.
-// Identifier la route et le verbe utilisé
-server.on('request', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-    });
-    res.writeBody('<h1> Hello World </h1>');
-    res.end('It Works!')
-  }
-});
-
-// Step #4. Exercice.
-// Construire un webserbice REST qui permet de CREATE/LIST des Users (name: String, age: Int)
