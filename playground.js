3654dfa1792898afeed326df1107bd1e26b276e2
Julien Sarazin <julien.sarazin@me.com>
2017-03-13 15:21:42 +0100

    added raw CRUD without express

--- playground.js
+++ playground.js
@@ -1,33 +1,78 @@
 const http = require('http');
 const server = http.createServer();
-const path = '/users';
-const users = [];
+const port = 3000;
+const db = [];
+const userRoute = '/users'
+const User = require('./User');
 
-server.on('request', (request, response) => {
-  var body = [];
-  response.setHeader('Content-Type', 'application/json');
+console.log(`server listening on port ${port}`);
+server.listen(port);
 
-  if (request.url != path || (request.method != 'POST' && request.method != 'GET')) {
-    response.statusCode = 404;
-    return response.end();
-  }
 
-  if (request.method == 'GET') {
-    return response.end(JSON.stringify(users));
-  }
+server.on('request', function(request, response){
+	const method = request.method;
+	const data = [];
+	response.setHeader('Content-Type', 'application/json');
 
-  request.on('data', (chunk) => {
-    body.push(chunk);
-  })
+	if (request.url != userRoute) {
+		response.statusCode = 404;
+		response.end();
+		return;
+	}
 
-  request.on('end', () => {
-    let data = Buffer.concat(body).toString();
-    let json = JSON.parse(data);
+	if (method == 'GET') {
+		response.end(JSON.stringify(db));
+		return;
+	}
 
-    users.push(json);
-    response.end(data);
-  });
+	request.on('data', function(chunk){
+		data.push(chunk);
+	});
+
+	request.on('end', function(){
+		let jsonString = data.toString();
+		const json = JSON.parse(jsonString)
+
+		if (method == 'POST') {
+			let user = new User(json)
+			db.push(user);
+			response.end(jsonString);
+			return;
+		}
+
+		if (method == 'PUT') {
+			for (let user of db) {
+					if (user.id == json.id) {
+						user.name = json.name;
+						response.end(JSON.stringify(user));
+						return;
+					}
+			}
+
+			response.statusCode = 404;
+			response.end();
+			return;
+		}
+
+		if (method == 'DELETE') {
+			let index = -1;
+
+			db.forEach((user, currentIndex)=> {
+				if (user.id == json.id) {
+					index = currentIndex;
+				}
+			});
+
+			if (index != -1) {
+				db.splice(index, 1);
+				response.statusCode = 204;
+				response.end();
+				return;
+			}
+
+			response.statusCode = 404;
+			response.end();
+			return;
+		}
+	});
 });
-
-console.log("server listening on port 3000");
-server.listen(3000);
