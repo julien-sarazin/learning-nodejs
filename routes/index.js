5a51dedb72b80e52fd30dbf4098b7ccdd9560936
Julien Sarazin <julien.sarazin@me.com>
2017-04-04 16:49:44 +0200

    updated logger to log response status code

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 module.exports = (api) => {
-  api.use(api.middlewares.logger);
-  api.use('/users', require('./users')(api));
-  api.use('/cars', require('./cars')(api));
+    api.use('*', api.middlewares.logger);
+    api.use('/users', require('./users')(api));
+    api.use('/cars', require('./cars')(api));
 };
