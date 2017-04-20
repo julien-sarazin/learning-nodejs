91bf8ceba477223d0d270c8a0712a0dcdd7b1a3e
Julien Sarazin <julien.sarazin@me.com>
2017-04-20 16:27:42 +0200

    added client

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./models')(server);                console.log('loading models...');
 require('./middlewares')(server);           console.log('loading middlewares...');
 require('./actions')(server);               console.log('loading actions...');
 require('./routes')(server);                console.log('loading routes...');
+require('./static')(server);                console.log('loading client...');
 
 console.log(`Server listening on port ${server.settings.port}`);
 server.listen(server.settings.port);
