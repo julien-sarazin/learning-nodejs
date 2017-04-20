ff9a26a727903d5ead3fbfdcb99828d59a75f950
Julien Sarazin <julien.sarazin@me.com>
2017-04-20 16:20:28 +0200

    updated production settings

--- settings/index.js
+++ settings/index.js
@@ -1,4 +1,9 @@
 module.exports = (api) => {
-  api.settings = require('./settings.json');
+    api.settings = require('./settings.json');
+
+    if (process.env.NODE_ENV == 'production') {
+        api.settings.port = process.env.PORT;
+        api.settings.db.url = process.env.MONGODB_URI;
+    }
 };
 
