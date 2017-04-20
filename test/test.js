a4ba77915d4e0f1ef0bd9058c7b4ab8b0a52873c
Julien Sarazin <julien.sarazin@me.com>
2017-04-20 16:50:33 +0200

    added faulty test case

--- test/test.js
+++ test/test.js
@@ -9,4 +9,12 @@ describe('GET /user', function () {
             .expect('Content-Type', /json/)
             .expect(200, done);
     });
+
+    it('should fail', function (done) {
+        request(server)
+            .get('/bots')
+            .set('Accept', 'application/json')
+            .expect(false)
+            .expect(200, done);
+    })
 });
